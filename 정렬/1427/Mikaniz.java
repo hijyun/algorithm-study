94df7e24d83681fc1540edc4bb8317d334d1f872
mikanji <k515jina@gmail.com>
2022-01-07 20:08:02 +0900

    class 이름 수정

--- 정렬/1427/Mikaniz.java
+++ 정렬/1427/Mikaniz.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
-public class mikaniz {
+public class Mikaniz {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
