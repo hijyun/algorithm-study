763b971597a8f6dae653b98c639ee5f3a1e022a7
Soooyeon-Kim <kob04124@gmail.com>
2022-01-12 19:40:35 +0900

    주석 추가

--- 정렬/2750/sooyeon.py
+++ 정렬/2750/sooyeon.py
@@ -1,4 +1,5 @@
 # 선택 정렬
+
 n = int(input())
 m = []
 for i in range(n):
@@ -6,12 +7,14 @@ for i in range(n):
     
 def selection_sort(lst):
     n = len(lst)
-    for i in range(0,n-1):
+    for i in range(0,n-1): # 0부터 n -2까지 반복
+        # i번 위치부터 끝까지 자료 값 중 최솟값의 위치를 찾음
         min_idx = i
         for j in range(i+1, n):
             if lst[j] < lst[min_idx]:
                 min_idx = j
-        lst[i], lst[min_idx] = lst[min_idx], lst[i]
+        # 찾은 최솟값을 i번 위치로 swap
+        lst[i], lst[min_idx] = lst[min_idx], lst[i] # 변수에 대입된 값을 교환
         # print(lst)     # 정렬 과정 출력하기
     print(*lst)
     
