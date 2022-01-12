0b3a3fcffc0f7592a09fb14b91b82784c3a86396
Ji Hyun <wus570708@naver.com>
2022-01-12 16:57:01 +0900

    1-2wk 정렬 개인문제 7795

--- 정렬/3273/hijyun.py
+++ 정렬/3273/hijyun.py
@@ -7,12 +7,11 @@ left, right = 0, n - 1
 count = 0
 tmp = 0
 while left != right:
-
     tmp = array[left] + array[right]
-
     if tmp == x:
         count += 1
         left += 1
+        right -= 1
     elif tmp > x:
         right -= 1
     else:
